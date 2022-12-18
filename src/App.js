19ebd3baad604e634fe6c6fe4b831ffd4c750a03
Leo Teixeira <teixeiraleo72@gmail.com>
2022-12-18 13:21:09 +0100

    mise en place de la todolist, ajout plus suppression

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
+  }
+
+  function handleChange(e) {
+    setNewTodo(e.target.value);
+  }
+
+  function handleDelete(index) {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todolist</h1>
-        <p>test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-      <body></body>
+      <body>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newTodo} onChange={handleChange} />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      </body>
     </div>
-  );
+  );  
 }
 
 export default App;
